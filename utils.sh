70d223672d6e8780e154389924c6cbe032aeab1c
booyaa <booyaabooyaabooyaa@gmail.com>
2016-02-11 19:10:13 +0000

    tighten up find funcs

--- utils.sh
+++ utils.sh
@@ -4,9 +4,17 @@
 # utility script, don't use directly #
 ######################################
 
-findcontainer() {
-  docker ps -qa --filter=name=$@
+# TODO: create funcs that return only one cid
+# TODO: how do we reference arrays in bash?
+findcontainers() {
+  docker ps -qa --filter="name=$@"
 }
+
+
+findrunningcontainers() {
+  docker ps -qa --filter="name=$@" --filter="status=running"
+}
+
 
 log() {
   #logger -s tagname $@
