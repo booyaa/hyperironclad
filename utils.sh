eb1bd57c9373e835f048e3895e28da1050e39a8f
booyaa <booyaabooyaabooyaa@gmail.com>
2016-02-15 19:40:53 +0000

    add file type guesser

--- utils.sh
+++ utils.sh
@@ -4,6 +4,7 @@
 # utility script, don't use directly #
 ######################################
 
+# docker
 # TODO: create funcs that return only one cid
 # TODO: how do we reference arrays in bash?
 findcontainers() {
@@ -15,7 +16,24 @@ findrunningcontainers() {
   docker ps -qa --filter="name=$@" --filter="status=running"
 }
 
+# misc
+isfiletype() {
+  # returns 1 if found otherwise 0
 
+  FILE=$1
+  shift
+  TERM=$@
+  # echo $FILE $TERM
+  # e.g. gzip compressed data, from Unix, last modified: Wed Feb 10 08:17:48 2016
+  FILE_OUTPUT=$(file --print0 --no-pad --brief $FILE | cut -f1 -d',') 
+  echo $FILE_OUTPUT | grep --quiet "$TERM" 
+  if [ $? -eq 0 ];
+  then echo 1
+  else echo 0
+  fi
+}
+
+# infra
 log() {
   #logger -s tagname $@
   echo $@
